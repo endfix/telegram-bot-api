ae8b4c83451ed5587eea3c18e64e78787c479f93
Obi <resik@xaker.ru>
2024-11-18 00:12:11 +0300

    added documentation for types of Getting Updates

--- Telegram.BotAPI/Types/GettingUpdates/Update.cs
+++ Telegram.BotAPI/Types/GettingUpdates/Update.cs
@@ -6,56 +6,146 @@ using Telegram.BotAPI.Types.Payments;
 
 namespace Telegram.BotAPI.Types.GettingUpdates;
 
+/// <summary>
+/// This object represents an incoming update.
+/// At most one of the optional parameters can be present in any given update.
+/// </summary>
 public sealed class Update
 {
+    /// <summary>
+    /// The update's unique identifier. Update identifiers start from a certain positive number and increase sequentially. 
+    /// This identifier becomes especially handy if you're using <see href="https://core.telegram.org/bots/api#setwebhook">webhooks</see>, 
+    /// since it allows you to ignore repeated updates or to restore the correct update sequence, 
+    /// should they get out of order. If there are no new updates for at least a week, then identifier of the next update will be chosen randomly instead of sequentially.
+    /// </summary>
     public long UpdateId { get; set; }
 
+    /// <summary>
+    /// Optional. New incoming message of any kind - text, photo, sticker, etc.
+    /// </summary>
     public Message Message { get; set; }
 
+    /// <summary>
+    /// Optional. New version of a message that is known to the bot and was edited. 
+    /// This update may at times be triggered by changes to message fields that are either unavailable or not actively used by your bot.
+    /// </summary>
     public Message EditedMessage { get; set; }
 
+    /// <summary>
+    /// Optional. New incoming channel post of any kind - text, photo, sticker, etc.
+    /// </summary>
     public Message ChannelPost { get; set; }
 
+    /// <summary>
+    /// Optional. New version of a channel post that is known to the bot and was edited. 
+    /// This update may at times be triggered by changes to message fields that are either unavailable or not actively used by your bot.
+    /// </summary>
     public Message EditedChannelPost { get; set; }
 
+    /// <summary>
+    /// Optional. The bot was connected to or disconnected from a business account, or a user edited an existing connection with the bot
+    /// </summary>
     public BusinessConnection BusinessConnection { get; set; }
 
+    /// <summary>
+    /// Optional. New message from a connected business account
+    /// </summary>
     public Message BusinessMessage { get; set; }
 
+    /// <summary>
+    /// Optional. New version of a message from a connected business account
+    /// </summary>
     public Message EditedBusinessMessage { get; set; }
 
+    /// <summary>
+    /// Optional. Messages were deleted from a connected business account
+    /// </summary>
     public BusinessMessagesDeleted DeletedBusinessMessages { get; set; }
 
+    /// <summary>
+    /// Optional. A reaction to a message was changed by a user. The bot must be an administrator 
+    /// in the chat and must explicitly specify "message_reaction" in the list of allowed_updates to receive these updates. 
+    /// The update isn't received for reactions set by bots.
+    /// </summary>
     public MessageReactionUpdated MessageReaction { get; set; }
 
+    /// <summary>
+    /// Optional. Reactions to a message with anonymous reactions were changed. The bot must be an administrator 
+    /// in the chat and must explicitly specify "message_reaction_count" in the list of allowed_updates to receive these updates. 
+    /// The updates are grouped and can be sent with delay up to a few minutes.
+    /// </summary>
     public MessageReactionCountUpdated MessageReactionCount { get; set; }
 
+    /// <summary>
+    /// Optional. New incoming <see href="https://core.telegram.org/bots/api#inline-mode">inline</see> query
+    /// </summary>
     public InlineQuery InlineQuery { get; set; }
 
+    /// <summary>
+    /// Optional. The result of an <see href="https://core.telegram.org/bots/api#inline-mode">inline</see> query that was chosen by a user and sent to their chat partner. 
+    /// Please see our documentation on the <see href="https://core.telegram.org/bots/inline#collecting-feedback">feedback collecting</see> for details on how to enable these updates for your bot.
+    /// </summary>
     public ChosenInlineResult ChosenInlineResult { get; set; }
 
+    /// <summary>
+    /// Optional. New incoming callback query
+    /// </summary>
     public CallbackQuery CallbackQuery { get; set; }
 
+    /// <summary>
+    /// Optional. New incoming shipping query. Only for invoices with flexible price
+    /// </summary>
     public ShippingQuery ShippingQuery { get; set; }
 
+    /// <summary>
+    /// Optional. New incoming pre-checkout query. Contains full information about checkout
+    /// </summary>
     public PreCheckoutQuery PreCheckoutQuery { get; set; }
 
+    /// <summary>
+    /// Optional. A user purchased paid media with a non-empty payload sent by the bot in a non-channel chat
+    /// </summary>
     public PaidMediaPurchased PurchasedPaidMedia { get; set; }
 
+    /// <summary>
+    /// Optional. New poll state. Bots receive only updates about manually stopped polls and polls, which are sent by the bot
+    /// </summary>
     public Poll Poll { get; set; }
 
+    /// <summary>
+    /// Optional. A user changed their answer in a non-anonymous poll. Bots receive new votes only in polls that were sent by the bot itself.
+    /// </summary>
     public PollAnswer PollAnswer { get; set; }
 
+    /// <summary>
+    /// Optional. The bot's chat member status was updated in a chat. For private chats, 
+    /// this update is received only when the bot is blocked or unblocked by the user.
+    /// </summary>
     public ChatMemberUpdated MyChatMember { get; set; }
 
+    /// <summary>
+    /// Optional. A chat member's status was updated in a chat.
+    /// The bot must be an administrator in the chat and must explicitly specify "chat_member" in the list of allowed_updates to receive these updates.
+    /// </summary>
     public ChatMemberUpdated ChatMember { get; set; }
 
+    /// <summary>
+    /// Optional. A request to join the chat has been sent.
+    /// The bot must have the can_invite_users administrator right in the chat to receive these updates.
+    /// </summary>
     public ChatJoinRequest ChatJoinRequest { get; set; }
 
+    /// <summary>
+    /// Optional. A chat boost was added or changed. The bot must be an administrator in the chat to receive these updates.
+    /// </summary>
     public ChatBoostUpdated ChatBoost { get; set; }
 
+    /// <summary>
+    /// Optional. A boost was removed from a chat. The bot must be an administrator in the chat to receive these updates.
+    /// </summary>
     public ChatBoostRemoved RemovedChatBoost { get; set; }
 
+    #region Custom helper methods
     public bool IsMessage() { return Message is not null; }
 
     public bool IsEditedMessage() { return EditedMessage is not null; }
@@ -101,6 +191,7 @@ public sealed class Update
     public bool IsChatBoost() { return ChatBoost is not null; }
 
     public bool IsRemovedChatBoost() { return RemovedChatBoost is not null; }
+    #endregion
 
     public static class Types
     {
--- Telegram.BotAPI/Types/GettingUpdates/WebhookInfo.cs
+++ Telegram.BotAPI/Types/GettingUpdates/WebhookInfo.cs
@@ -2,23 +2,53 @@ using System.Collections.Generic;
 
 namespace Telegram.BotAPI.Types.GettingUpdates;
 
-public class WebhookInfo
+/// <summary>
+/// Describes the current status of a webhook.
+/// </summary>
+public sealed class WebhookInfo
 {
+    /// <summary>
+    /// Webhook URL, may be empty if webhook is not set up
+    /// </summary>
     public string Url { get; set; }
 
+    /// <summary>
+    /// True, if a custom certificate was provided for webhook certificate checks
+    /// </summary>
     public bool HasCustomCertificate { get; set; }
 
+    /// <summary>
+    /// Number of updates awaiting delivery
+    /// </summary>
     public int PendingUpdateCount { get; set; }
 
+    /// <summary>
+    /// Optional. Currently used webhook IP address
+    /// </summary>
     public string IpAddress { get; set; }
 
+    /// <summary>
+    /// Optional. Unix time for the most recent error that happened when trying to deliver an update via webhook
+    /// </summary>
     public int LastErrorDate { get; set; }
 
+    /// <summary>
+    /// Optional. Error message in human-readable format for the most recent error that happened when trying to deliver an update via webhook
+    /// </summary>
     public string LastErrorMessage { get; set; }
 
+    /// <summary>
+    /// Optional. Unix time of the most recent error that happened when trying to synchronize available updates with Telegram datacenters
+    /// </summary>
     public int LastSynchronizationErrorDate { get; set; }
 
+    /// <summary>
+    /// Optional. The maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery
+    /// </summary>
     public int MaxConnections { get; set; }
 
+    /// <summary>
+    /// Optional. A list of update types the bot is subscribed to. Defaults to all update types except chat_member
+    /// </summary>
     public List<string> AllowedUpdates { get; set; }
 }

f22bebbad38603e5e23fb31ff240bea8b6f12c2b
obi <resik@xaker.ru>
2026-09-06 16:41:08 +0300

    Document Telegram Passport method

--- Telegram.BotAPI/Extensions/BotApiClientExtensions.TelegramPassport.cs
+++ Telegram.BotAPI/Extensions/BotApiClientExtensions.TelegramPassport.cs
@@ -9,12 +9,27 @@ namespace Endfix.Telegram.BotAPI.Extensions;
 
 public static partial class BotApiClientExtensions
 {
+    /// <summary>
+    /// Reports errors in Telegram Passport data submitted by a user.
+    /// </summary>
+    /// <param name="client">Bot API client used to send the request.</param>
+    /// <param name="parameters">User identifier and Passport validation errors.</param>
+    /// <param name="cancellationToken">Token used to cancel the request.</param>
+    /// <returns><see langword="true"/> on success.</returns>
     public static async Task<bool> SetPassportDataErrorsAsync(
         this IBotApiClient client, 
         SetPassportDataErrorsParameters parameters, 
         CancellationToken cancellationToken = default)
         => await client.ExecuteAsync<bool>(new ApiRequest("setPassportDataErrors", parameters), cancellationToken);
 
+    /// <summary>
+    /// Reports errors in Telegram Passport data submitted by a user.
+    /// </summary>
+    /// <param name="client">Bot API client used to send the request.</param>
+    /// <param name="userId">User identifier.</param>
+    /// <param name="errors">Passport errors that the user must resolve before resubmitting the data.</param>
+    /// <param name="cancellationToken">Token used to cancel the request.</param>
+    /// <returns><see langword="true"/> on success.</returns>
     public static async Task<bool> SetPassportDataErrorsAsync(
         this IBotApiClient client,
         long userId,
--- Telegram.BotAPI/Parameters/TelegramPassport/SetPassportDataErrorsParameters.cs
+++ Telegram.BotAPI/Parameters/TelegramPassport/SetPassportDataErrorsParameters.cs
@@ -9,7 +9,9 @@ namespace Endfix.Telegram.BotAPI.Parameters;
 /// </summary>
 public sealed class SetPassportDataErrorsParameters : ApiRequestParameters
 {
+    /// <summary>User identifier.</summary>
     public required long UserId { get; init; }
 
+    /// <summary>Passport errors that the user must resolve before resubmitting the data.</summary>
     public required IReadOnlyList<PassportElementError> Errors { get; init; }
 }

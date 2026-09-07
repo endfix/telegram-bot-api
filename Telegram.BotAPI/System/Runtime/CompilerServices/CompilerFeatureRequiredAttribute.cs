429dc0eabd1e94037c7c928ffb58146b22d89fe2
obi <resik@xaker.ru>
2026-09-07 16:02:05 +0300

    Document compiler compatibility attributes

--- Telegram.BotAPI/System/Runtime/CompilerServices/CompilerFeatureRequiredAttribute.cs
+++ Telegram.BotAPI/System/Runtime/CompilerServices/CompilerFeatureRequiredAttribute.cs
@@ -1,5 +1,7 @@
 ﻿namespace System.Runtime.CompilerServices;
 
+/// <summary>Indicates that a compiler feature is required to use the attributed program element.</summary>
+/// <param name="featureName">Name of the required compiler feature.</param>
 [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
 #pragma warning disable CS9113 // Параметр не прочитан.
 public sealed class CompilerFeatureRequiredAttribute(string featureName) : Attribute
--- Telegram.BotAPI/System/Runtime/CompilerServices/RequiredMemberAttribute.cs
+++ Telegram.BotAPI/System/Runtime/CompilerServices/RequiredMemberAttribute.cs
@@ -1,5 +1,6 @@
 ﻿namespace System.Runtime.CompilerServices;
 
+/// <summary>Indicates that a type or member participates in the required-member feature.</summary>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
 public sealed class RequiredMemberAttribute : Attribute
 {

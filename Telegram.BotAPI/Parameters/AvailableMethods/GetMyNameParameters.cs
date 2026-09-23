cfaa5ce15fde576063faf98ff4f3a7747eb6cf72
obi <resik@xaker.ru>
2026-09-24 02:08:44 +0300

    Seal GetMyNameParameters
    Match the sealed request-parameter types used by the rest of the client.

--- Telegram.BotAPI/Parameters/AvailableMethods/GetMyNameParameters.cs
+++ Telegram.BotAPI/Parameters/AvailableMethods/GetMyNameParameters.cs
@@ -5,7 +5,7 @@ namespace Endfix.Telegram.BotAPI.Parameters;
 /// <summary>
 /// Parameters for the <c>getMyName</c> method.
 /// </summary>
-public class GetMyNameParameters : ApiRequestParameters
+public sealed class GetMyNameParameters : ApiRequestParameters
 {
     /// <summary>Two-letter ISO 639-1 language code or an empty string.</summary>
     public string? LanguageCode { get; init; }
